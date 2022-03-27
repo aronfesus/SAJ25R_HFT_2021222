041df358b6dacf2db374e65ff9b4a5108598e413
aronf <aronf@DESKTOP-3Q639PG>
2022-03-27 11:57:00 +0200

    Add project files.

---
+++ SAJ25R_HFT_2021222.Endpoint/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SAJ25R_HFT_2021222.Endpoint
+{
+    public class Class1
+    {
+    }
+}
---
+++ SAJ25R_HFT_2021222.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SAJ25R_HFT_2021222.Logic
+{
+    public class Class1
+    {
+    }
+}
