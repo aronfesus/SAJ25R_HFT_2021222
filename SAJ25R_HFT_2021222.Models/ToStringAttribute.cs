f285ce69685f2567434ca484e621ed054e6d2c78
aronf <aronf@DESKTOP-3Q639PG>
2022-04-29 11:35:23 +0200

    to string attribute created

---
+++ SAJ25R_HFT_2021222.Models/ToStringAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAJ25R_HFT_2021222.Models
+{
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ToStringAttribute : Attribute
+    {
+    }
+}
